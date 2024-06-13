14f237bda674b42aa1ff9463a7130bf828025c69
Luis Fernando <111304852+Srluis-1@users.noreply.github.com>
2024-06-13 18:38:30 -0300

    termino do cartão de receitas salgadas
    termino do cartão de receitas salgadas  e todos os botões dos cartões funcionando

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,6 @@ const btnAvancar = document.getElementById("btn-avancar")
  const cartoes = document.querySelectorAll(".cartao")
  const btnVoltar = document.getElementById("btn-voltar")
 
-
  btnAvancar.addEventListener("click", function(){
    const ehUltimoCartao = cartaoAtual === cartoes.length - 1
    if(ehUltimoCartao) return
@@ -20,14 +19,62 @@ const btnAvancar = document.getElementById("btn-avancar")
    esconderCartaoSelecionado();
     cartaoAtual--;
     mostrarCartao();
+
  })
 
 
 function mostrarCartao() {
+
    cartoes[cartaoAtual].classList.add("selecionado");
+
 }
 
 function esconderCartaoSelecionado() {
    const cartaoSelecionado = document.querySelector(".selecionado");
    cartaoSelecionado.classList.remove("selecionado");
 }
+
+
+
+
+const btnAvancarMassas = document.getElementById("btn-avancar1")
+ let cartaoAtualMassas = 0;
+ const cartoesMassa = document.querySelectorAll(".cartao-massas")
+ const btnVoltarMassas = document.getElementById("btn-voltar1")
+
+ btnAvancarMassas.addEventListener("click", function(){
+   const ehUltimoCartaoMassas = cartaoAtualMassas === cartoes.length - 1
+   if(ehUltimoCartaoMassas) return
+
+   esconderCartaoSelecionadoMassas();
+    cartaoAtualMassas++;
+    mostrarCartaoMassas();
+ })
+
+ btnVoltarMassas.addEventListener("click", function(){
+   const ehPrimeiroCartaoMassas = cartaoAtualMassas === 0 
+   if(ehPrimeiroCartaoMassas) return
+
+   esconderCartaoSelecionadoMassas();
+    cartaoAtualMassas--;
+    mostrarCartaoMassas();
+
+ })
+
+
+function mostrarCartaoMassas() {
+
+   cartoesMassa[cartaoAtualMassas].classList.add("selecionado-massas");
+   
+}
+
+function esconderCartaoSelecionadoMassas() {
+   const esconderCartaoSelecionadoMassas = document.querySelector(".selecionado-massas");
+   esconderCartaoSelecionadoMassas.classList.remove("selecionado-massas");
+}
+
+
+
+
+
+
