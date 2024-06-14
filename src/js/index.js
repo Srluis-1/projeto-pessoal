cbee37d18e43cc8b21413923e428d0fbf718b701
Luis Fernando <111304852+Srluis-1@users.noreply.github.com>
2024-06-14 14:09:03 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,27 @@
 const btnAvancar = document.getElementById("btn-avancar")
- let cartaoAtual = 0;
- const cartoes = document.querySelectorAll(".cartao")
- const btnVoltar = document.getElementById("btn-voltar")
+let cartaoAtual = 0;
+const cartoes = document.querySelectorAll(".cartao")
+const btnVoltar = document.getElementById("btn-voltar")
 
- btnAvancar.addEventListener("click", function(){
+
+btnAvancar.addEventListener("click", function () {
    const ehUltimoCartao = cartaoAtual === cartoes.length - 1
-   if(ehUltimoCartao) return
+   if (ehUltimoCartao) return
 
    esconderCartaoSelecionado();
-    cartaoAtual++;
-    mostrarCartao();
- })
+   cartaoAtual++;
+   mostrarCartao();
+})
 
- btnVoltar.addEventListener("click", function(){
-   const ehPrimeiroCartao = cartaoAtual === 0 
-   if(ehPrimeiroCartao) return
+btnVoltar.addEventListener("click", function () {
+   const ehPrimeiroCartao = cartaoAtual === 0
+   if (ehPrimeiroCartao) return
 
    esconderCartaoSelecionado();
-    cartaoAtual--;
-    mostrarCartao();
+   cartaoAtual--;
+   mostrarCartao();
 
- })
+})
 
 
 function mostrarCartao() {
@@ -36,36 +37,36 @@ function esconderCartaoSelecionado() {
 
 
 
-
 const btnAvancarMassas = document.getElementById("btn-avancar1")
- let cartaoAtualMassas = 0;
- const cartoesMassa = document.querySelectorAll(".cartao-massas")
- const btnVoltarMassas = document.getElementById("btn-voltar1")
+let cartaoAtualMassas = 0;
+const cartoesMassa = document.querySelectorAll(".cartao-massas")
+const btnVoltarMassas = document.getElementById("btn-voltar1")
 
- btnAvancarMassas.addEventListener("click", function(){
+
+btnAvancarMassas.addEventListener("click", function () {
    const ehUltimoCartaoMassas = cartaoAtualMassas === cartoes.length - 1
-   if(ehUltimoCartaoMassas) return
+   if (ehUltimoCartaoMassas) return
 
    esconderCartaoSelecionadoMassas();
-    cartaoAtualMassas++;
-    mostrarCartaoMassas();
- })
+   cartaoAtualMassas++;
+   mostrarCartaoMassas();
+})
 
- btnVoltarMassas.addEventListener("click", function(){
-   const ehPrimeiroCartaoMassas = cartaoAtualMassas === 0 
-   if(ehPrimeiroCartaoMassas) return
+btnVoltarMassas.addEventListener("click", function () {
+   const ehPrimeiroCartaoMassas = cartaoAtualMassas === 0
+   if (ehPrimeiroCartaoMassas) return
 
    esconderCartaoSelecionadoMassas();
-    cartaoAtualMassas--;
-    mostrarCartaoMassas();
+   cartaoAtualMassas--;
+   mostrarCartaoMassas();
 
- })
+})
 
 
 function mostrarCartaoMassas() {
 
    cartoesMassa[cartaoAtualMassas].classList.add("selecionado-massas");
-   
+
 }
 
 function esconderCartaoSelecionadoMassas() {
@@ -74,7 +75,77 @@ function esconderCartaoSelecionadoMassas() {
 }
 
 
+const btnAvancarDoce = document.getElementById("btn-avancar2")
+let cartaoAtualDoce = 0;
+const cartoesDoce = document.querySelectorAll(".cartao-doce")
+const btnVoltarDoce = document.getElementById("btn-voltar2")
+
+btnAvancarDoce.addEventListener("click", function () {
+   const ehUltimoCartaoDoce = cartaoAtualDoce === cartoes.length - 1
+   if (ehUltimoCartaoDoce) return
+
+   esconderCartaoSelecionadoDoce();
+   cartaoAtualDoce++;
+   mostrarCartaoDoce();
+})
+
+btnVoltarDoce.addEventListener("click", function () {
+   const ehPrimeiroCartaoDoce = cartaoAtualDoce === 0
+   if (ehPrimeiroCartaoDoce) return
+
+   esconderCartaoSelecionadoDoce();
+   cartaoAtualDoce--;
+   mostrarCartaoDoce();
+
+})
+
+
+function mostrarCartaoDoce() {
+
+   cartoesDoce[cartaoAtualDoce].classList.add("selecionado-doces");
+
+}
+
+function esconderCartaoSelecionadoDoce() {
+   const esconderCartaoSelecionadoDoce = document.querySelector(".selecionado-doces");
+   esconderCartaoSelecionadoDoce.classList.remove("selecionado-doces");
+}
 
 
 
+const btnAvancarPudin = document.getElementById("btn-avancar3")
+let cartaoAtualPudin = 0;
+const cartoesPudin = document.querySelectorAll(".cartao-pudin")
+const btnVoltarPudin = document.getElementById("btn-voltar3")
 
+
+btnAvancarPudin.addEventListener("click", function () {
+   const ehUltimoCartaoPudin = cartaoAtualPudin === cartoes.length - 1
+   if (ehUltimoCartaoPudin) return
+
+   esconderCartaoSelecionadoPudin();
+   cartaoAtualPudin++;
+   mostrarCartaoPudin();
+})
+
+btnVoltarPudin.addEventListener("click", function () {
+   const ehPrimeiroCartaoPudin = cartaoAtualPudin === 0
+   if (ehPrimeiroCartaoPudin) return
+
+   esconderCartaoSelecionadoPudin();
+   cartaoAtualPudin--;
+   mostrarCartaoPudin();
+
+})
+
+
+function mostrarCartaoPudin() {
+
+   cartoesPudin[cartaoAtualPudin].classList.add("selecionado-pudin");
+
+}
+
+function esconderCartaoSelecionadoPudin() {
+   const esconderCartaoSelecionadoPudin = document.querySelector(".selecionado-pudin");
+   esconderCartaoSelecionadoPudin.classList.remove("selecionado-pudin");
+}
